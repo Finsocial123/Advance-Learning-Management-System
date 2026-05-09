85fdee909e477540505c4f09dca6684c59a24da8
Shahbaj Sheikh <thisisshahbaj@gmail.com>
2026-05-09 13:28:18 +0530

    fixed the models relationship issue

--- backend/app/models/enrollment.py
+++ backend/app/models/enrollment.py
@@ -9,7 +9,7 @@ class Enrollment(Base):
 
     id = Column(Integer, primary_key=True, index=True)
 
-    student_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    student_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
     course_id = Column(Integer, ForeignKey("courses.id"), nullable=False)
 
     progress = Column(Float, default=0.0, nullable=False)
